91ff15590354cf0a92156877a7fc5b6b98dcf7e8
hyun6ik <fkanwm@naver.com>
2021-03-02 01:19:54 +0900

    프로젝트 생성

---
+++ querydsl_repeat/src/test/java/com/example/querydsl_repeat/QuerydslRepeatApplicationTests.java
@@ -0,0 +1,41 @@
+package com.example.querydsl_repeat;
+
+import com.example.querydsl_repeat.entity.Hello;
+import com.example.querydsl_repeat.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class QuerydslRepeatApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+}
