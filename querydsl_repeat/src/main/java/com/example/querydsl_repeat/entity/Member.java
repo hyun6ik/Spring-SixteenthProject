abb428392b2c169f4036a364c30a80e1ebfe734a
hyun6ik <fkanwm@naver.com>
2021-03-02 23:42:25 +0900

    시작 - JPQL vs Querydsl

--- querydsl_repeat/src/main/java/com/example/querydsl_repeat/entity/Member.java
+++ querydsl_repeat/src/main/java/com/example/querydsl_repeat/entity/Member.java
@@ -17,24 +17,24 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    private String userName;
+    private String username;
     private int age;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name ="team_id")
     private Team team;
 
-    public Member(String userName) {
-        this.userName = userName;
+    public Member(String username) {
+        this.username = username;
     }
 
-    public Member(String userName, int age) {
-        this.userName = userName;
+    public Member(String username, int age) {
+        this.username = username;
         this.age = age;
     }
 
-    public Member(String userName, int age, Team team) {
-        this.userName = userName;
+    public Member(String username, int age, Team team) {
+        this.username = username;
         this.age = age;
         if (team != null){
             changeTeam(team);
