c07e7ed255989d9ac48ee7d7c5cff75fb63dce30
hyun6ik <fkanwm@naver.com>
2021-03-02 23:27:55 +0900

    예제 도메인 모델과 동작확인

---
+++ querydsl_repeat/src/main/java/com/example/querydsl_repeat/entity/Member.java
@@ -0,0 +1,51 @@
+package com.example.querydsl_repeat.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","username", "age"})
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String userName;
+    private int age;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name ="team_id")
+    private Team team;
+
+    public Member(String userName) {
+        this.userName = userName;
+    }
+
+    public Member(String userName, int age) {
+        this.userName = userName;
+        this.age = age;
+    }
+
+    public Member(String userName, int age, Team team) {
+        this.userName = userName;
+        this.age = age;
+        if (team != null){
+            changeTeam(team);
+        } else {
+            this.team = team;
+        }
+    }
+
+    // 연관관계 메서드
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
